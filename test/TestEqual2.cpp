477249d51617a165640ce45b38e04ecc49f180cb
Dimo Markov <dimomarkov@gmail.com>
2023-09-16 11:45:29 +0300

    Split a test cpp to avoid clang-cl running out of memory is 32bit build

--- test/TestEqual2.cpp
+++ test/TestEqual2.cpp
@@ -76,14 +76,10 @@ LANGULUS(INLINED) void ControlEqualV(const Vector<LHS, C>& lhsArray, const Vecto
 
 ///																									
 TEMPLATE_TEST_CASE("Compare equality", "[compare]"
-   , NUMBERS_ALL()
-   , VECTORS_ALL(1)
-   , VECTORS_ALL(2)
-   , VECTORS_ALL(3)
-   , VECTORS_ALL(4)
-   , VECTORS_ALL(5)
-   , VECTORS_ALL(8)
-   , VECTORS_ALL(9)
+   , VECTORS_ALL(16)
+   , VECTORS_ALL(17)
+   , VECTORS_ALL(32)
+   , VECTORS_ALL(33)
 ) {
    using T = TestType;
 
