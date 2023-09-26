71ed3eef925fd9f890aa0c7bbfda7ff34274a752
Dimo Markov <dimomarkov@gmail.com>
2023-09-26 22:24:21 +0300

    Reduced test case

--- test/TestStrangeClangBug.cpp
+++ test/TestStrangeClangBug.cpp
@@ -9,7 +9,7 @@
 #include "Main.hpp"
 #include <catch2/catch.hpp>
 
-#if LANGULUS_SIMD(256BIT)
+#if LANGULUS_SIMD(AVX) or LANGULUS_SIMD(AVX2)
 
 TEST_CASE("Strange clang-cl bug", "[bug]") {
    GIVEN("x + y = r") {
@@ -30,6 +30,32 @@ TEST_CASE("Strange clang-cl bug", "[bug]") {
             _mm256_store_si256(reinterpret_cast<__m256i*>(yy), y);
             _mm256_store_si256(reinterpret_cast<__m256i*>(rr), r);
             _mm256_store_si256(reinterpret_cast<__m256i*>(ctrl), control);
+
+            REQUIRE(0 == memcmp(rr, ctrl, 32));
+         }
+      }
+   }
+}
+
+TEST_CASE("Strange clang-cl bug (simde equivalent)", "[bug]") {
+   GIVEN("x + y = r") {
+      simde__m256i x = simde_mm256_setr_epi64x(1, 2, 3, 4);
+      simde__m256i y = simde_mm256_setr_epi64x(7, 8, 9, 0);
+      simde__m256i control = simde_mm256_setr_epi64x(8, 10, 12, 4);
+
+      WHEN("Added") {
+         simde__m256i r = simde_mm256_add_epi64(x, y);
+
+         THEN("The result should be correct") {
+            alignas(32) std::uint64_t xx[4];
+            alignas(32) std::uint64_t yy[4];
+            alignas(32) std::uint64_t rr[4];
+            alignas(32) std::uint64_t ctrl[4];
+
+            simde_mm256_store_si256(reinterpret_cast<simde__m256i*>(xx), x);
+            simde_mm256_store_si256(reinterpret_cast<simde__m256i*>(yy), y);
+            simde_mm256_store_si256(reinterpret_cast<simde__m256i*>(rr), r);
+            simde_mm256_store_si256(reinterpret_cast<simde__m256i*>(ctrl), control);
 
             REQUIRE(0 == memcmp(rr, ctrl, 32));
          }
