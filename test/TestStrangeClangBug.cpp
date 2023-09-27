cc2699653468b504fc4b132872205f7c6fd754f2
Epixu <dimomarkov@gmail.com>
2023-09-27 18:36:11 +0300

    Improved test

--- test/TestStrangeClangBug.cpp
+++ test/TestStrangeClangBug.cpp
@@ -121,14 +121,20 @@ TEST_CASE("Strange clang-cl bug (langulus vector equivalent)", "[bug]") {
       Vector<std::int64_t, 3> ctrlsrc1;
       Vector<std::int64_t, 3> ctrlsrc2;
 
-      WHEN("Added") {
+      WHEN("Added as array") {
          SIMD::Add(xsrc.mArray, ysrc.mArray, ctrlsrc1.mArray);
-         SIMD::Add(xsrc, ysrc, ctrlsrc2);
 
          THEN("The result should be correct") {
             REQUIRE(ctrlsrc1[0] == xsrc[0] + ysrc[0]);
             REQUIRE(ctrlsrc1[1] == xsrc[1] + ysrc[1]);
             REQUIRE(ctrlsrc1[2] == xsrc[2] + ysrc[2]);
+         }
+      }
+
+      WHEN("Added as vector") {
+         SIMD::Add(xsrc, ysrc, ctrlsrc2);
+
+         THEN("The result should be correct") {
             REQUIRE(ctrlsrc2[0] == xsrc[0] + ysrc[0]);
             REQUIRE(ctrlsrc2[1] == xsrc[1] + ysrc[1]);
             REQUIRE(ctrlsrc2[2] == xsrc[2] + ysrc[2]);
