5ee1b713d94572c2421470b4dae5a14ce4b7c220
Epixu <dimomarkov@gmail.com>
2025-02-06 12:56:54 +0200

    Attempt to work around MSVC ICE

--- source/Fill.hpp
+++ source/Fill.hpp
@@ -23,10 +23,10 @@ namespace Langulus::SIMD
       #if LANGULUS_SIMD(128BIT)
          if constexpr (R <= 16) {
             using T = Decvq<TypeOf<decltype(s)>>;
-            if      constexpr (CT::Integer8<T>)    return V128<T> {simde_mm_set1_epi8        (reinterpret_cast<const int8_t&> (GetFirst(s)))};
-            else if constexpr (CT::Integer16<T>)   return V128<T> {simde_mm_set1_epi16       (reinterpret_cast<const int16_t&>(GetFirst(s)))};
-            else if constexpr (CT::Integer32<T>)   return V128<T> {simde_mm_set1_epi32       (reinterpret_cast<const int32_t&>(GetFirst(s)))};
-            else if constexpr (CT::Integer64<T>)   return V128<T> {simde_mm_set1_epi64x      (reinterpret_cast<const int64_t&>(GetFirst(s)))};
+            if      constexpr (CT::Integer8<T>)    return V128<T> {simde_mm_set1_epi8        (static_cast<int8_t> (GetFirst(s)))};
+            else if constexpr (CT::Integer16<T>)   return V128<T> {simde_mm_set1_epi16       (static_cast<int16_t>(GetFirst(s)))};
+            else if constexpr (CT::Integer32<T>)   return V128<T> {simde_mm_set1_epi32       (static_cast<int32_t>(GetFirst(s)))};
+            else if constexpr (CT::Integer64<T>)   return V128<T> {simde_mm_set1_epi64x      (static_cast<int64_t>(GetFirst(s)))};
             else if constexpr (CT::Float<T>)       return V128<T> {simde_mm_broadcast_ss     (&GetFirst(s))};
             else if constexpr (CT::Double<T>)      return V128<T> {simde_mm_set1_pd          ( GetFirst(s))};
             else static_assert(false, "Unsupported type for filling V128");
@@ -37,10 +37,10 @@ namespace Langulus::SIMD
       #if LANGULUS_SIMD(256BIT)
          if constexpr (R <= 32) {
             using T = Decvq<TypeOf<decltype(s)>>;
-            if      constexpr (CT::Integer8<T>)    return V256<T> {simde_mm256_set1_epi8     (reinterpret_cast<const int8_t&> (GetFirst(s)))};
-            else if constexpr (CT::Integer16<T>)   return V256<T> {simde_mm256_set1_epi16    (reinterpret_cast<const int16_t&>(GetFirst(s)))};
-            else if constexpr (CT::Integer32<T>)   return V256<T> {simde_mm256_set1_epi32    (reinterpret_cast<const int32_t&>(GetFirst(s)))};
-            else if constexpr (CT::Integer64<T>)   return V256<T> {simde_mm256_set1_epi64x   (reinterpret_cast<const int64_t&>(GetFirst(s)))};
+            if      constexpr (CT::Integer8<T>)    return V256<T> {simde_mm256_set1_epi8     (static_cast<int8_t> (GetFirst(s)))};
+            else if constexpr (CT::Integer16<T>)   return V256<T> {simde_mm256_set1_epi16    (static_cast<int16_t>(GetFirst(s)))};
+            else if constexpr (CT::Integer32<T>)   return V256<T> {simde_mm256_set1_epi32    (static_cast<int32_t>(GetFirst(s)))};
+            else if constexpr (CT::Integer64<T>)   return V256<T> {simde_mm256_set1_epi64x   (static_cast<int64_t>(GetFirst(s)))};
             else if constexpr (CT::Float<T>)       return V256<T> {simde_mm256_broadcast_ss  (&GetFirst(s))};
             else if constexpr (CT::Double<T>)      return V256<T> {simde_mm256_broadcast_sd  (&GetFirst(s))};
             else static_assert(false, "Unsupported type for filling of V256");
@@ -51,10 +51,10 @@ namespace Langulus::SIMD
       #if LANGULUS_SIMD(512BIT)
          if constexpr (R <= 64) {
             using T = Decvq<TypeOf<decltype(s)>>;
-            if      constexpr (CT::Integer8<T>)    return V512<T> {simde_mm512_set1_epi8     (GetFirst(s))};
-            else if constexpr (CT::Integer16<T>)   return V512<T> {simde_mm512_set1_epi16    (GetFirst(s))};
-            else if constexpr (CT::Integer32<T>)   return V512<T> {simde_mm512_set1_epi32    (GetFirst(s))};
-            else if constexpr (CT::Integer64<T>)   return V512<T> {simde_mm512_set1_epi64    (GetFirst(s))};
+            if      constexpr (CT::Integer8<T>)    return V512<T> {simde_mm512_set1_epi8     (static_cast<int8_t> (GetFirst(s)))};
+            else if constexpr (CT::Integer16<T>)   return V512<T> {simde_mm512_set1_epi16    (static_cast<int16_t>(GetFirst(s)))};
+            else if constexpr (CT::Integer32<T>)   return V512<T> {simde_mm512_set1_epi32    (static_cast<int32_t>(GetFirst(s)))};
+            else if constexpr (CT::Integer64<T>)   return V512<T> {simde_mm512_set1_epi64    (static_cast<int64_t>(GetFirst(s)))};
             else if constexpr (CT::Float<T>)       return V512<T> {simde_mm512_set1_ps       (GetFirst(s))};
             else if constexpr (CT::Double<T>)      return V512<T> {simde_mm512_set1_pd       (GetFirst(s))};
             else static_assert(false, "Unsupported type for filling of V512");
