a3abef586bac57c318d664bf988850265f0b5cbe
Epixu <dimomarkov@gmail.com>
2025-02-07 16:19:59 +0200

    Attempt to work around MSVC ICE

--- test/Multiply/TestMul-VV.cpp
+++ test/Multiply/TestMul-VV.cpp
@@ -141,7 +141,7 @@ TEMPLATE_TEST_CASE("Vector * Vector", "[multiply]"
          #endif
       }
 
-      /*WHEN("Multiplied in reverse (with saturation)") {
+      WHEN("Multiplied in reverse (with saturation)") {
          ControlMul<true>(y, x, rCheck);
          SIMD::Multiply<true>(y, x, r);
 
@@ -153,6 +153,6 @@ TEMPLATE_TEST_CASE("Vector * Vector", "[multiply]"
          SIMD::Multiply<false>(y, x, r);
 
          REQUIRE(r == rCheck);
-      }*/
+      }
    }
 }
