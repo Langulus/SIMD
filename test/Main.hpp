3881fde453f506e5b5f74e9413bccfdd8387312c
Epixu <dimomarkov@gmail.com>
2025-02-07 13:23:37 +0200

    Attempt to work around MSVC ICE

--- test/Main.hpp
+++ test/Main.hpp
@@ -69,12 +69,13 @@ using namespace Langulus;
    (Vector<::std::int64_t, S>), \
    VECTORS_REAL(S)
 
-#define VECTORS_UNSIGNED(S)     \
-   (Vector<::std::uint8_t, S>), \
+#define VECTORS_UNSIGNED(S)   (Vector<Byte, S>)           
+
+   /*(Vector<::std::uint8_t, S>), \
    (Vector<::std::uint16_t, S>),\
    (Vector<::std::uint32_t, S>),\
    (Vector<::std::uint64_t, S>),\
-   (Vector<Byte, S>)           
+   (Vector<Byte, S>)  */         
    /*(Vector<char8_t, S>),        \
    (Vector<char16_t, S>),       \
    (Vector<char32_t, S>),       \
