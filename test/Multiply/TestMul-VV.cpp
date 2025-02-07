c83b6e7cecc5a452efcb3be4424f765ef1787ee9
Epixu <dimomarkov@gmail.com>
2025-02-07 02:16:16 +0200

    Attempt to work around MSVC ICE

--- test/Multiply/TestMul-VV.cpp
+++ test/Multiply/TestMul-VV.cpp
@@ -13,10 +13,10 @@ TEMPLATE_TEST_CASE("Vector * Vector", "[multiply]"
    , VECTORS_ALL(1)
    , VECTORS_ALL(2)
    , VECTORS_ALL(3)
-   , VECTORS_ALL(4)*/
-   , VECTORS_ALL(5)
-   /*, VECTORS_ALL(8)
-   , VECTORS_ALL(9)
+   , VECTORS_ALL(4)
+   , VECTORS_ALL(5)*/
+   , VECTORS_ALL(8)
+   /*, VECTORS_ALL(9)
    , VECTORS_ALL(16)
    , VECTORS_ALL(17)
    , VECTORS_ALL(32)
