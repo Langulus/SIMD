bbf40bffca13d597433c290ffd533bb805a6eb0d
Epixu <dimomarkov@gmail.com>
2025-02-07 17:21:30 +0200

    Attempt to work around MSVC ICE

--- test/Multiply/TestMul-VV.cpp
+++ test/Multiply/TestMul-VV.cpp
@@ -33,13 +33,13 @@ TEMPLATE_TEST_CASE("Vector * Vector", "[multiply]"
          InitOne(y, -5);
       }
 
-      WHEN("Multiplied as constexpr (with saturation)") {
+      //WHEN("Multiplied as constexpr (with saturation)") {
          static_assert(SIMD::Multiply<true>(T {0}, T {5}) == T {0});
-      }
+      //}
 
-      WHEN("Multiplied as constexpr (without saturation)") {
+      //WHEN("Multiplied as constexpr (without saturation)") {
          static_assert(SIMD::Multiply<false>(T {0}, T {5}) == T {0});
-      }
+      //}
 
       WHEN("Multiplied (with saturation)") {
          ControlMul<true>(x, y, rCheck);
